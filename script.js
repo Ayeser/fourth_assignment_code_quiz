b699e0a3957a4ea5d46e698750e8c849f3f05474
Matthew Wester <MatthewWester@live.com>
2019-12-10 20:55:15 -0700

    Start button disappears and framework for quiz questions beginning

--- script.js
+++ script.js
@@ -5,21 +5,64 @@
 
 //use assignment 18 to make each answer a clickable event. and if the clicked equals the answer key in the object, voila. if answer === choices[id]
 // add if statement so when reaches end it finishes game
+// listEl.addEventListener("click", function(event) {
+//         event.preventDefault();
+//         if(event.target.matches("button")) {
+//           const item = document.createElement("div");
+//           item.textContent = groceries[event.target.parentElement.id];
+//           shoppingCartEl.append(item);
+//         }
+//       });
 
 $(document).ready(function () {
 
-        function runQuiz() {
-                console.log("second hiya");
-                for(i=0;i<questions.length;i++) {
-                        $("#quiz").append("<h2>" + questions[i] + "</h2>");
-                        console.log(questions);
-                }
-        }
+var i = 0;
 
-        const startGameButton = $("#clickToStart").on('click', function () {
+function showQuestion() {
+        console.log("Questions length is " + questions.length)
+        if (i = questions.length) {
+                endOfGame();
+        } else {
+        $("#quiz").append("<h2>" + questions[i].title + "</h2>");
+        $("#quiz").append('<div id="a">' + '<button type="button" class="btn btn-primary">' + questions[i].choices[0] + '</button>' + '</div>');
+        $("#quiz").append('<div id="b">' + '<button type="button" class="btn btn-primary">' + questions[i].choices[1] + '</button>' + '</div>');
+        $("#quiz").append('<div id="c">' + '<button type="button" class="btn btn-primary">' + questions[i].choices[2] + '</button>' + '</div>');
+        $("#quiz").append('<div id="d">' + '<button type="button" class="btn btn-primary">' + questions[i].choices[3] + '</button>' + '</div>');
+        i = i + 1;
+        console.log(i);
+}}
+
+function endOfGame() {
+        console.log("The end");
+}
+
+        // function runQuiz() {
+        //         //This loop creates the questions in the quiz 
+        //         for(i=0;i<questions.length;i++) {
+        //                 $("#quiz").append("<h2>" + questions[i].title + "</h2>");
+        //         $("#quiz").append('<div id="a">' + '<button type="button" class="btn btn-primary">' + questions[i].choices[0] + '</button>' + '</div>');
+        //         $("#quiz").append('<div id="b">' + '<button type="button" class="btn btn-primary">' + questions[i].choices[1] + '</button>' + '</div>');
+        //         $("#quiz").append('<div id="c">' + '<button type="button" class="btn btn-primary">' + questions[i].choices[2] + '</button>' + '</div>');
+        //         $("#quiz").append('<div id="d">' + '<button type="button" class="btn btn-primary">' + questions[i].choices[3] + '</button>' + '</div>');
+                
+        //         if ($("#a").click() && questions[i].answer === questions[i].choices[0]) {
+        //                 console.log("A is correct!");
+        //         } else if ($("#b").click() && questions[i].answer === questions[i].choices[1]) {
+        //                 console.log("B is correct!");
+        //         } else if ($("#c").click() && questions[i].answer === questions[i].choices[2]) {
+        //                 console.log("C is correct!");
+        //         } else if ($("#d").click() && questions[i].answer === questions[i].choices[3]) {
+        //                 console.log("D is correct!");
+        //         }
+
+               
+        //         }
+        // };
+
+        $("#clickToStart").on('click', function () {
                 console.log("hiya");
-                $("#startGameSection").hide("slow");
-                runQuiz();
+                $("#clickToStart").hide("slow");
+                showQuestion();
         });
 
 
